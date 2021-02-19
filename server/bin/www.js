1717cdf9b2182e82a8824d2b3586054e149402ae
Joyce Wu <jianjoycewu@google.com>
2021-02-18 16:01:58 -0800

    minor tweek

--- server/bin/www.js
+++ server/bin/www.js
@@ -2,9 +2,9 @@
 const http = require('http')
 const { serverHandle } = require('../app')
 
-const PORT = 2000
+const PORT = 8000
 const server = http.createServer(serverHandle)
 
-server.listen(2000)
-
-console.log('app is runing at port 2000')
+server.listen(PORT, () => {
+  console.log('app is runing at port ' + PORT)
+})
